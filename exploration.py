01bc0ee731d6404819731cc211cfe0396d822ddd
ardalan77400@gmail.com <ardalan.mehrani@iosquare.com>
2016-05-13 17:15:42 +0200

    - avito exploration

---
+++ exploration.py
@@ -0,0 +1,30 @@
+__author__ = 'Ardalan'
+
+FOLDER = "/home/ardalan/Documents/kaggle/avito/"
+
+import os, sys, time, re, collections, operator, copy, itertools, zipfile
+import pandas as pd
+import numpy as np
+
+
+def loadFileinZipFile(zip_filename, dtypes=None, parsedate = None, password=None, **kvargs):
+        """
+        Load file to dataframe.
+        """
+        with zipfile.ZipFile(zip_filename, 'r') as myzip:
+            if password:
+                myzip.setpassword(password)
+
+            inside_zip_filename = myzip.filelist[0].filename
+
+            if parsedate:
+                pd_data = pd.read_csv(myzip.open(inside_zip_filename), sep=',', parse_dates=parsedate, dtype=dtypes, **kvargs)
+            else:
+                pd_data = pd.read_csv(myzip.open(inside_zip_filename), sep=',', dtype=dtypes, **kvargs)
+            return pd_data, inside_zip_filename
+
+
+pdCategory, _ = loadFileinZipFile(FOLDER + "data/Category.csv.zip")
+pdItemPairs_train, _ = loadFileinZipFile(FOLDER + "data/ItemPairs_train.csv.zip")
+
+pdItemInfo_train, _ = loadFileinZipFile(FOLDER + "data/ItemInfo_train.csv.zip")
