38b3b1491df3a146f14bf1e5bfe199ac83f5d1f4
Ardalan Mehrani <ardalan.mehrani@iosquare.com>
2016-05-18 18:24:54 +0200

    - CV vs LB weird differences

--- prepro/createDataset.py
+++ prepro/createDataset.py
@@ -31,74 +31,71 @@ def haversine(pddata, lon1='', lat1='', lon2='', lat2=''):
 pdtrain1 = pd.read_hdf(FOLDER + "data/train_merged-part1.h")
 pdtrain2 = pd.read_hdf(FOLDER + "data/train_merged-part2.h")
 pdtrain = pdtrain1.append(pdtrain2)
+pdtest = pd.read_hdf(FOLDER + "data/test_merged.h")
+pd_data = pdtrain.append(pdtest)
+
 del pdtrain1
 del pdtrain2
 
-pdtrain['title_1'].fillna("", inplace=True)
-pdtrain['title_2'].fillna("", inplace=True)
-pdtrain['description_1'].fillna("", inplace=True)
-pdtrain['description_2'].fillna("", inplace=True)
-pdtrain['price_1'].fillna(-9999, inplace=True)
-pdtrain['price_2'].fillna(-9999, inplace=True)
-pdtrain['attrsJSON_1'].fillna("", inplace=True)
-pdtrain['attrsJSON_2'].fillna("", inplace=True)
-pdtrain['images_array_1'].fillna("", inplace=True)
-pdtrain['images_array_2'].fillna("", inplace=True)
 
-# feature dataframe
-pd_features = pd.DataFrame()
+def create_D1(pd_data):
 
-# title
-pd_features['title_difflen'] = np.abs(pdtrain['title_2'].apply(len) - pdtrain['title_1'].apply(len))
-pd_features['title_levenshtein'] = pd.read_hdf(FOLDER + "data/feat_train_title_leven.h")
-pd_features['title_Dlevenshtein'] = pd.read_hdf(FOLDER + "data/feat_train_title_demarauleven.h")
-pd_features['title_jaro'] = pd.read_hdf(FOLDER + "data/feat_train_title_jaro.h")
-pd_features['title_jarowinkler'] = pd.read_hdf(FOLDER + "data/feat_train_title_jarowinkler.h")
-pd_features['title_hamming'] = pd.read_hdf(FOLDER + "data/feat_train_title_hamming.h")
+    pd_data['title_1'].fillna("", inplace=True)
+    pd_data['title_2'].fillna("", inplace=True)
+    pd_data['description_1'].fillna("", inplace=True)
+    pd_data['description_2'].fillna("", inplace=True)
+    pd_data['price_1'].fillna(-9999, inplace=True)
+    pd_data['price_2'].fillna(-9999, inplace=True)
+    pd_data['attrsJSON_1'].fillna("", inplace=True)
+    pd_data['attrsJSON_2'].fillna("", inplace=True)
+    pd_data['images_array_1'].fillna("", inplace=True)
+    pd_data['images_array_2'].fillna("", inplace=True)
 
-# description
-pd_features['description_difflen'] = np.abs(pdtrain['description_2'].apply(len) - pdtrain['description_1'].apply(len))
-pd_features['description_levenshtein'] = pd.read_hdf(FOLDER + "data/feat_train_description_leven.h")
-pd_features['description_Dlevenshtein'] = pd.read_hdf(FOLDER + "data/feat_train_description_demarauleven.h")
-pd_features['description_jaro'] = pd.read_hdf(FOLDER + "data/feat_train_description_jaro.h")
-pd_features['description_jarowinkler'] = pd.read_hdf(FOLDER + "data/feat_train_description_jarowinkler.h")
-pd_features['description_hamming'] = pd.read_hdf(FOLDER + "data/feat_train_description_hamming.h")
+    # feature dataframe
+    pd_features = pd.DataFrame()
 
-# price
-pd_features['price_diff_abs'] = np.abs(pdtrain['price_1'] - pdtrain['price_2'])
-pd_features['price_diff_square'] = np.square(pdtrain['price_1'] - pdtrain['price_2'])
+    # title
+    pd_features['title_difflen'] = np.abs(pd_data['title_2'].apply(len) - pd_data['title_1'].apply(len))
+    pd_features['title_levenshtein'] = pd.read_hdf(FOLDER + "data/feat_train_title_leven.h")
+    pd_features['title_Dlevenshtein'] = pd.read_hdf(FOLDER + "data/feat_train_title_demarauleven.h")
+    pd_features['title_jaro'] = pd.read_hdf(FOLDER + "data/feat_train_title_jaro.h")
+    pd_features['title_jarowinkler'] = pd.read_hdf(FOLDER + "data/feat_train_title_jarowinkler.h")
+    pd_features['title_hamming'] = pd.read_hdf(FOLDER + "data/feat_train_title_hamming.h")
 
-# attrJson
-pd_features['attrsJSON_difflen'] = np.abs(pdtrain['attrsJSON_1'].apply(len) - pdtrain['attrsJSON_2'].apply(len))
+    # description
+    pd_features['description_difflen'] = np.abs(pd_data['description_2'].apply(len) - pd_data['description_1'].apply(len))
+    pd_features['description_levenshtein'] = pd.read_hdf(FOLDER + "data/feat_train_description_leven.h")
+    pd_features['description_Dlevenshtein'] = pd.read_hdf(FOLDER + "data/feat_train_description_demarauleven.h")
+    pd_features['description_jaro'] = pd.read_hdf(FOLDER + "data/feat_train_description_jaro.h")
+    pd_features['description_jarowinkler'] = pd.read_hdf(FOLDER + "data/feat_train_description_jarowinkler.h")
+    pd_features['description_hamming'] = pd.read_hdf(FOLDER + "data/feat_train_description_hamming.h")
 
-# images_array
-pd_features['images_diff_number'] = np.abs(pdtrain['images_array_1'].apply(lambda x: len(x.split(','))) -
-                                       pdtrain['images_array_2'].apply(lambda x: len(x.split(','))))
+    # price
+    pd_features['price_diff_abs'] = np.abs(pd_data['price_1'] - pd_data['price_2'])
+    pd_features['price_diff_square'] = np.square(pd_data['price_1'] - pd_data['price_2'])
 
-# geographic features
-pd_features['metroID_same'] = 1 * (pdtrain['metroID_1'] == pdtrain['metroID_2'])
-pd_features['locationID_same'] = 1 * (pdtrain['locationID_1'] == pdtrain['locationID_2'])
-pd_features['regionID_same'] = 1 * (pdtrain['regionID_1'] == pdtrain['regionID_2'])
-pd_features['haversine'] = haversine(pdtrain, lon1='lon_1', lat1='lat_1', lon2='lon_2', lat2='lat_2')
-pd_features['haversine'] = (pd_features['haversine'] - pd_features['haversine'].mean()) / pd_features['haversine'].std()
+    # attrJson
+    pd_features['attrsJSON_difflen'] = np.abs(pd_data['attrsJSON_1'].apply(len) - pd_data['attrsJSON_2'].apply(len))
 
-# CATEGORY, PARENT CATEGORY
-pd_features['categoryID_same'] = 1 * (pdtrain['categoryID_1'] == pdtrain['categoryID_2'])
-pd_features['parentCategoryID_same'] = 1 * (pdtrain['parentCategoryID_1'] == pdtrain['parentCategoryID_2'])
+    # images_array
+    pd_features['images_diff_number'] = np.abs(pd_data['images_array_1'].apply(lambda x: len(x.split(','))) -
+                                           pd_data['images_array_2'].apply(lambda x: len(x.split(','))))
 
+    # geographic features
+    pd_features['metroID_same'] = 1 * (pd_data['metroID_1'] == pd_data['metroID_2'])
+    pd_features['locationID_same'] = 1 * (pd_data['locationID_1'] == pd_data['locationID_2'])
+    pd_features['regionID_same'] = 1 * (pd_data['regionID_1'] == pd_data['regionID_2'])
+    pd_features['haversine'] = haversine(pd_data, lon1='lon_1', lat1='lat_1', lon2='lon_2', lat2='lat_2')
+    pd_features['haversine'] = (pd_features['haversine'] - pd_features['haversine'].mean()) / pd_features['haversine'].std()
 
+    # CATEGORY, PARENT CATEGORY
+    pd_features['categoryID_same'] = 1 * (pd_data['categoryID_1'] == pd_data['categoryID_2'])
+    pd_features['parentCategoryID_same'] = 1 * (pd_data['parentCategoryID_1'] == pd_data['parentCategoryID_2'])
 
-Y = pdtrain['isDuplicate'].values
-X = np.array(pd_features)
+    pd_features['isDuplicate'] = pd_data['isDuplicate']
+    pd_features['id'] = pd_data['id_1']
 
+    return pd_features
 
-from xgboost import XGBClassifier
-from sklearn.cross_validation import train_test_split
-xtrain, xtest, ytrain, ytest= train_test_split(X, Y, test_size=0.20, random_state=42)
-clf = XGBClassifier(max_depth=6, learning_rate=0.1, n_estimators=1000, objective="binary:logistic",
-                    nthread=8)
-
-clf.fit(xtrain, ytrain, eval_set=[(xtest, ytest)], eval_metric="auc", early_stopping_rounds=100)
-
-
-
+D1 = create_D1(pd_data)
+D1.to_hdf(FOLDER + "data/D1_18may.p", 'w')
